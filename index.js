bc6147ea453c75d80680d365e95a9a061cd03e4c
Levy Chemelil <levy.chelal@student.moringaschool.com>
2024-06-26 13:01:14 +0300

    Adds index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function returnsANamedFunction() {
+    function namedFunction() {
+      console.log("This is a named function");
+    }
+    
+    return namedFunction;
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return () => {
+      console.log("This is an anonymous function");
+    };
+  }
+  
+  module.exports = {
+    receivesAFunction,
+    returnsANamedFunction,
+    returnsAnAnonymousFunction
+  };
+  
